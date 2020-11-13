8f32bbb6fb840bf3c4b77799b38c19552b60439b
Hyunwoo Jang <4everwrz@gmail.com>
2020-11-13 23:35:04 +0900

    Update refresh.js

--- refresh.js
+++ refresh.js
@@ -1,1 +1,5 @@
-function autoRefresh_div() { var currentLocation = window.location; $("#premium_exchange_form").load(currentLocation + ' #premium_exchange_form); } setInterval('autoRefresh_div()', 3000);function autoRefresh_div() { var currentLocation = window.location; $("#premium_exchange_form").load(currentLocation + ' #premium_exchange_form); } setInterval('autoRefresh_div()', 1000);
+function autoRefresh_div() {
+  var currentLocation = window.location;
+  $("#premium_exchange_form").load(currentLocation + ' #premium_exchange_form);
+}
+setInterval('autoRefresh_div()', 1000);
