698ebaafc1c52e756fdbbb79203e754e5bc854ab
Hyunwoo Jang <4everwrz@gmail.com>
2020-11-13 23:38:08 +0900

    Update refresh.js

--- refresh.js
+++ refresh.js
@@ -1,5 +1,6 @@
 function autoRefresh_div() {
   var currentLocation = window.location;
   $("#premium_exchange_form").load(currentLocation + ' #premium_exchange_form);
+  alert('refresh');
 }
 setInterval('autoRefresh_div()', 1000);
