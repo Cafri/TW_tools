ce56acf05b61d658eb787e51a8555f185be207f7
Hyunwoo Jang <4everwrz@gmail.com>
2020-11-13 23:28:28 +0900

    Update and rename Noname1.js to refresh.js

---
+++ refresh.js
@@ -0,0 +1,1 @@
+function autoRefresh_div() { var currentLocation = window.location; $("#premium_exchange_form").load(currentLocation + ' #premium_exchange_form); } setInterval('autoRefresh_div()', 3000);function autoRefresh_div() { var currentLocation = window.location; $("#premium_exchange_form").load(currentLocation + ' #premium_exchange_form); } setInterval('autoRefresh_div()', 3000);
